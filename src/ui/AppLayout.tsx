e7ccd00a7fe45012534cf79a2601c62a2445723c
Eslam-Elwey <eslam.elwey@gmail.com>
2026-05-15 23:54:48 +0300

    remove unused React to fix deployment and add env file to gitignore

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -2,6 +2,9 @@ import { Outlet } from "react-router";
 import Sidebar from "./Sidebar";
 
 export default function AppLayout() {
+
+  console.log(import.meta.env.VITE_BASE_URL);
+
   return (
     <div className="min-h-screen">
       <div className="flex h-full">
