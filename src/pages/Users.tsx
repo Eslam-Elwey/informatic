8a41633568d95ccf8254dab73ba1b17ca78cad02
Eslam-Elwey <eslam.elwey@gmail.com>
2026-05-17 04:11:39 +0300

    edit in gap value in users page

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -3,7 +3,7 @@ import SearchBar from "../ui/SearchBar";
 
 export default function Users() {
   return (
-    <div className="flex flex-col gap-3 mt-3">
+    <div className="flex flex-col gap-8 mt-3">
       <div className="flex justify-evenly items-center px-6">
         <h2 className="p-4 text-2xl">Users</h2>
         <SearchBar resourceName="name" />
